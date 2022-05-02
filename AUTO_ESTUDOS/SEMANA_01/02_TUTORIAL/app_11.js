7081282f1879bb530df872304573efaff28dc696
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-05-02 08:23:44 -0300

    Atualizacoes

--- AUTO_ESTUDOS/SEMANA_01/02_TUTORIAL/app_11.js
+++ AUTO_ESTUDOS/SEMANA_01/02_TUTORIAL/app_11.js
@@ -7,8 +7,9 @@ const server = http.createServer((req, res) => {
   res.end('<!DOCTYPE html> <head> <title>Etapa 0 - INSTALACAO</title></head> \
               <body>\
                 <div id="main"> \
-                       <h1> Etapa 1 - INSTALACAO - Servidor Node.js </h1> \
-                       <H2> Meu servidor NODE.js funciona!</H2> </div> \
+                       <h1> <Font COLOR = "GREEN" FONT FACE = "Calibri"> Motivos para viver:</FONT> </h1> \
+                       <H2> Gatinhos. Apenas.</H2> </div> \
+                       <H2> Cachorrinhos tambem.</H3> \
               </body> \
            </html>');
 });
---
+++ AUTO_ESTUDOS/SEMANA_02/02_TUTORIAL/backend/app_23.js
@@ -0,0 +1,23 @@
+
+const express = require('express'); 
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3023;
+var contador = 0;
+app.use(express.static("../frontend/"));
+
+app.get('/conta', (req, res) => {
+  contador +=1;
+	res.write("<h1> Chamando servidor - Contador = </h1> <h3>");
+  res.write(String(contador)+"</h3>");
+
+  res.write("<p> <a href=\"http://127.0.0.1:3023/conta\">");
+  res.write("<input type=\"button\" value=\"Vai Servidor\" />");
+  res.write("</a>");   
+	res.end("");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
